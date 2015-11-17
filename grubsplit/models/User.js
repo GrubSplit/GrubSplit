065a62e4346d62e7101ebdd7601aaf69cc0b4a99
mpertierra <marcosp@mit.edu>
2015-11-17 17:42:37 -0500

    Comment out gravatar helper method in User.js.

--- grubsplit/models/User.js
+++ grubsplit/models/User.js
@@ -3,7 +3,7 @@
 */
 
 var bcrypt = require('bcrypt');
-var crypto = require('crypto');
+// var crypto = require('crypto');
 var mongoose = require('mongoose');
 
 var userSchema = new mongoose.Schema({
@@ -59,11 +59,11 @@ userSchema.methods.comparePassword = function(candidatePassword, cb) {
 /**
  * Helper method for getting user's gravatar.
  */
-userSchema.methods.gravatar = function(size) {
-  if (!size) size = 200;
-  if (!this.email) return 'https://gravatar.com/avatar/?s=' + size + '&d=retro';
-  var md5 = crypto.createHash('md5').update(this.email).digest('hex');
-  return 'https://gravatar.com/avatar/' + md5 + '?s=' + size + '&d=retro';
-};
+// userSchema.methods.gravatar = function(size) {
+//   if (!size) size = 200;
+//   if (!this.email) return 'https://gravatar.com/avatar/?s=' + size + '&d=retro';
+//   var md5 = crypto.createHash('md5').update(this.email).digest('hex');
+//   return 'https://gravatar.com/avatar/' + md5 + '?s=' + size + '&d=retro';
+// };
 
 module.exports = mongoose.model('User', userSchema);
