d9e7813f91ebb51256b88b0cffef8b2fd8f88a77
Matthew Miklasevich <mattmik@mit.edu>
2015-11-17 11:49:55 -0500

    Completed Request library

--- grubsplit/libraries/Request.js
+++ grubsplit/libraries/Request.js
@@ -1,32 +1,38 @@
 /**
- * Send an Http Request
+ * Create an Http Request
  * @param {[type]}   method   Http method ('GET', 'POST')
  * @param {[type]}   url      endpoint for request
- * @param {[type]}   params   list of parameters containing a tuple (ex. ["user_id", 123])
+ * @param {[type]}   params   list of parameter tuples (ex. [["user_id", 123]])
  * @param {Function} callback Check if response was successful
- *
- * TODO: Add listeners to check if reqeust was successful
- *       Store response
  *
  * Author: mattmik
  */
 var Request = function (method, url, params, callback) {
   var that = Object.create(Request.prototype);
 
-  var request = new XMLHttpRequest();
-
   /**
-   * [send description]
-   * @return {[type]} [description]
+   * Send an Http Request
+   * @param  {XMLHttpRequest} request  To be sent
+   * @param  {boolean} async           True if request is async
    */
-  that.send = function () {
+  var send = function (request, async) {
     var form = new FormData();
     params.forEach(function (param) {
       form.append(param[0], param[1]);
     });
-    request.open(method, url);
-    req.send(form);
+
+    // Send response to callback when request is successful
+    request.onreadystatechange = function () {
+      if (request.readyState == 4 && request.status == 200) {
+        callback(request.responseText);
+      }
+    };
+
+    request.open(method, url, async);
+    reqest.send(form);
   };
+
+  send(new XMLHttpRequest(), true);
 
   Object.freeze(that);
   return that;
