145164eb7887d94e2435a7851b06f0a25f41a057
mpertierra <marcosp@mit.edu>
2015-11-18 15:28:01 -0500

    Removed commented out code.

--- grubsplit/models/User.js
+++ grubsplit/models/User.js
@@ -1,14 +1,14 @@
 /**
-* Boilerplate code from https://github.com/sahat/hackathon-starter/blob/master/models/User.js
+* Represents the User, also deals with authentication
+*
+* Author: marcosp
 */
 
-// var bcrypt = require('bcrypt');
 var mongoose = require('mongoose');
 var passportLocalMongoose = require('passport-local-mongoose');
 
 var userSchema = new mongoose.Schema({
   email: { type: String, unique: true, lowercase: true, required: true },
-  // password: { type: String, required: true }
   // tokens: Array,
   // profile: {
   //   name: { type: String, default: '' },
@@ -23,32 +23,5 @@ userSchema.plugin(passportLocalMongoose, {
   'usernameUnique': true,
   'usernameLowerCase': true
 });
-
-// /**
-//  * Password hash middleware.
-//  */
-// userSchema.pre('save', function(next) {
-//   var user = this;
-//   if (!user.isModified('password')) return next();
-//   bcrypt.genSalt(10, function(err, salt) {
-//     if (err) return next(err);
-//     bcrypt.hash(user.password, salt, function(err, hash) {
-//       if (err) return next(err);
-//       user.password = hash;
-//       next();
-//     });
-//   });
-// });
-
-/**
- * Helper method for validating user's password.
- */
-// userSchema.methods.comparePassword = function(candidatePassword, cb) {
-//   bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
-//     if (err) return cb(err);
-//     cb(null, isMatch);
-//   });
-// };
-
 
 module.exports = mongoose.model('User', userSchema);
--- grubsplit/routes/users.js
+++ grubsplit/routes/users.js
@@ -1,5 +1,9 @@
 /**
-* Boilerplate code from https://github.com/sahat/hackathon-starter/blob/master/controllers/user.js
+* Routes for user login/signup/logout 
+*
+* Author: marcosp
+*         Also, used some boilerplate code from 
+*         https://github.com/sahat/hackathon-starter/blob/master/controllers/user.js
 */
 var express = require('express');
 var router = express.Router();
@@ -23,30 +27,7 @@ router.get('/login', function(req, res) {
  */
 router.post('/login', passport.authenticate('local', { successRedirect: '/',
                                    failureRedirect: '/users/login',
-                                   failureFlash: 'Invalid email or password.' })); //FIXME: failureFlash not working?
-  // req.assert('email', 'Email is not valid').isEmail();
-  // req.assert('password', 'Password cannot be blank').notEmpty();
-
-  // var errors = req.validationErrors();
-
-  // if (errors) {
-  //   req.flash('errors', errors);
-  //   return res.redirect('users/login');
-  // }
-
-  // passport.authenticate('local', function(err, user, info) {
-  //   if (err) return next(err);
-  //   if (!user) {
-  //     req.flash('errors', { msg: info.message });
-  //     return res.redirect('users/login');
-  //   }
-  //   req.logIn(user, function(err) {
-  //     if (err) return next(err);
-  //     req.flash('success', { msg: 'Success! You are logged in.' });
-  //     res.redirect('/');
-  //   });
-  // })(req, res, next);
-// });
+                                   failureFlash: 'Invalid username or password.' })); //FIXME: failureFlash not working?
 
 /**
  * GET /users/logout
@@ -102,25 +83,6 @@ router.post('/signup', function(req, res, next) {
       });
     });
   });
-
-  // var user = new User({
-  //   email: req.body.email,
-  //   password: req.body.password
-  // });
-
-  // User.findOne({ email: req.body.email }, function(err, existingUser) {
-  //   if (existingUser) {
-  //     req.flash('errors', { msg: 'Account with that email address already exists.' });
-  //     return res.redirect('/users/signup');
-  //   }
-  //   user.save(function(err) {
-  //     if (err) return next(err);
-  //     req.logIn(user, function(err) {
-  //       if (err) return next(err);
-  //       res.redirect('/');
-  //     });
-  //   });
-  // });
 });
 
 module.exports = router;
