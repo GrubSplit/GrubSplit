a7666732376a295940e04372823730234da1bacc
Amanda Zhou <aezhou@mit.edu>
2015-11-18 16:52:33 -0500

    basic grub routing

--- grubsplit/routes/grubs.js
+++ grubsplit/routes/grubs.js
@@ -12,5 +12,28 @@ router.get('/:id', function(req, res) {
   res.render('/grubs', { grub: grub});
 });
 
+/**
+ * POST /grubs/:id
+ * Grub page.
+  Request body:
+  	- grubID: id of the current grub
+    - subgrub: the subgrub to be added
+  Response:
+    - success: true if the server succeeded in recording the user's freet
+    - err: on failure, an error message
+ */
+router.post('/:id', function(req, res) {
+  if (!req.user) return res.redirect('/login');
+
+  // TODO add subgrub to grub 
+  // Grub.addSubgrub(req.grubID, req.subgrub, function(grub) {
+  // 	res.render('/grubs', { grub: grub});
+  // })
+
+  // TODO pull grub from db
+
+  // res.render('/grubs', { grub: res.grub});
+});
+
 
 module.exports = router;
