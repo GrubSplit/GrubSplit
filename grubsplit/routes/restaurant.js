675d7899a2ba5a9505307d5310c0dead851652db
Amanda Zhou <aezhou@mit.edu>
2015-11-19 13:36:03 -0500

    removing requireOwnership from restaurant routes
    restaurant pages are not tied to a grub - removing for now

--- grubsplit/routes/restaurant.js
+++ grubsplit/routes/restaurant.js
@@ -4,24 +4,6 @@ var Restaurant = require('../models/Restaurant');
 var utils = require('../utils/utils');
 
 /*
-  Require ownership whenever accessing a particular grub
-  This means that the client accessing the resource must be logged in
-  as the user that originally created the grub. Clients who are not owners 
-  of this particular resource will receive a 404.
-  Why 404? We don't want to distinguish between grubs that don't exist at all
-  and grubs that exist but don't belong to the client. This way a malicious client
-  that is brute-forcing urls should not gain any information.
-*/
-var requireOwnership = function(req, res, next) {
-  // TODO: Does user need to be pulled from db (refreshed?)
-  if ( req.user.open_grubs.indexOf(req.grub._id) === -1  || req.user.past_grubs.indexOf(req.grub._id) === -1) {
-    utils.sendErrResponse(res, 404, 'Resource not found.');
-  } else {
-    next();
-  }
-};
-
-/*
   Grab a restaurant from the store whenever one is referenced with an ID in the
   request path (any routes defined with :restaurant as a paramter).
 */
@@ -39,7 +21,7 @@ router.param('restaurant', function(req, res, next, restaurantIdStr) {
 });
 
 // Require ownership
-router.all('/:restaurant', requireOwnership);
+// router.all('/:restaurant', requireOwnership);
 
 /**
  * GET /restaurant/:restaurant
