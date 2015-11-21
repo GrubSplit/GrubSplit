7ed192d1a005eccb5aed195bab466d212d9e67cc
Amanda Zhou <aezhou@mit.edu>
2015-11-21 18:01:45 -0500

    basic grub mongoose methods

--- grubsplit/models/Grub.js
+++ grubsplit/models/Grub.js
@@ -15,8 +15,89 @@
 var mongoose = require('mongoose');
 var ObjectID = mongoose.Schema.Types.ObjectId;
 
+var grubSchema = new mongoose.Schema({
+  owner: { 
+    type: ObjectID, 
+    ref: "User", 
+    required: true 
+  },
+  subGrubs: [{
+    type: ObjectID,
+    ref: "SubGrub"
+  }],
+  order_Open: {
+    type:Boolean,
+    default: true
+  },
+  restaurantID: Number,
+  tax: Number,
+  tip: Number,
+  delivery_fee: Number
+});
+
+/*
+  given a grubID, return the grub, with all subgrubs populated
+  @param: grubID = mongo ObjectID of grub
+  @param: callback(err, grub)
+*/
+grubSchema.statics.getGrub = function(grubID, callback) {
+  Grub.find({_id: grubID}).populate(['subGrubs']).exec(function(err, grub) {
+    if (grub) {
+      callback(null, grub);
+    } else {
+      callback({msg: 'could not find grub'});
+    }
+  });
+}
+
+/*
+  given a grub ID, update the tax, and return the grub or error message
+  @param: grubID = id of the grub
+  @param: tax = tax amount
+  @param: callback(err, grub)
+*/
+grubSchema.statics.updateTax = function(grubID, tax, callback) {
+  Grub.findOneAndUpdate({_id: grubID}, {$set: {tax: tax}}, {new: true}, function(err, grub) {
+    if (grub) {
+      callback(null, grub);
+    } else {
+      callback({msg: "could not find grub to update tax"});
+    }
+  });
+};
 
 
+/*
+  given a grub ID, update the delivery amount, and return the grub or error message
+  @param: grubID = id of the grub
+  @param: delivery = delivery fee
+  @param: callback(err, grub)
+*/
+grubSchema.statics.updateDelivery = function(grubID, delivery, callback) {
+  Grub.findOneAndUpdate({_id: grubID}, {$set: {delivery: delivery}}, {new: true}, function(err, grub) {
+    if (grub) {
+      callback(null, grub);
+    } else {
+      callback({msg: "could not find grub to update delivery fee"});
+    }
+  });
+};
+
+/*
+  given a grub ID, update the tip, and return the grub or error message
+  @param: grubID = id of the grub
+  @param: tip = tip amount
+  @param: callback(err, grub)
+*/
+grubSchema.statics.updateTip = function(grubID, tip, callback) {
+  Grub.findOneAndUpdate({_id: grubID}, {$set: {tip: tip}}, {new: true}, function(err, grub) {
+    if (grub) {
+      callback(null, grub);
+    } else {
+      callback({msg: "could not find grub to update tip"});
+    }
+  });
+};
 
 module.exports = mongoose.model('Grub', grubSchema);
 
