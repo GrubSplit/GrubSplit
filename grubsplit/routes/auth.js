bcd444ee77ef5e6ab43b52ca46e4193d8b70fa6c
Matthew Miklasevich <mattmik@mit.edu>
2015-11-21 13:02:06 -0500

    Update token in User model

--- grubsplit/routes/auth.js
+++ grubsplit/routes/auth.js
@@ -1,10 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var User = require('../models/User');
 
 router.get('/', function(req, res, next) {
   var query = req.query;
   // TODO: Store query.code token in user's database
+  User.update({
+    _id: req.user.id
+  }, {
+    $set: {
+      token: query.code
+    },
+  }, function (err) {
+    if (!err) {
+      console.log('UPDATED TOKEN');
+    }
+    // TODO: handle error
+  });
   console.log('QUERY: ' + query.code);
+  console.log(req.user.id);
   res.redirect('/');
 });
 
