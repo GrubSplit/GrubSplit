54bba3c57a6457a4a8e459679c846e6b51b31f9b
Amanda Zhou <aezhou@mit.edu>
2015-11-22 11:21:49 -0500

    creation routing pseudocode

--- grubsplit/routes/grubs.js
+++ grubsplit/routes/grubs.js
@@ -2,7 +2,6 @@ var express = require('express');
 var router = express.Router();
 var Grub = require('../models/Grub');
 var utils = require('../utils/utils');
-
 /*
   Require ownership whenever accessing a particular grub
   This means that the client accessing the resource must be logged in
@@ -26,13 +25,11 @@ var requireOwnership = function(req, res, next) {
   request path (any routes defined with :grub as a paramter).
 */
 router.param('grub', function(req, res, next, grubIdStr) {
-  if (grubIdStr === "0") { // temporary bypass logic -> remove this when grub id's are real
-    req.grub = [];
-    next();
-  } else {
-  var grubId = new ObjectID(grubIdStr);
-  // TODO: Implement this
-  Grub.getGrub(grubId, function(err, grub) {
+  // if (grubIdStr === "0") { // temporary bypass logic -> remove this when grub id's are real
+  //   req.grub = [];
+  //   next();
+  // } else {
+  Grub.getGrub(grubIdStr, function(err, grub) {
     if (grub) {
       req.grub = grub;
       next();
@@ -40,18 +37,35 @@ router.param('grub', function(req, res, next, grubIdStr) {
       utils.sendErrResponse(res, 404, 'Resource not found.');
     }
   });
-}
+// }
 });
 
 // Require ownership
 router.all('/:grub', requireOwnership);
+
+/*
+  POST /grubs
+  Request body: 
+    - restaurantID : TODO how to put here?
+  Response:
+*/
+router.post('/', function(req, res) {
+  req.restaurantID = '70706' //TODO FIX THIS SO IT'S REAL PLEASE
+  Grub.createNewGrub(req.session.passport.user, req.restaurantID, function(err, grub) {
+    if (err) {
+      //TODO
+    } else {
+      // GET /grubs/grub._Id
+    }
+  });
+});
 
 /**
  * GET /grubs/:grub
  * Grub page.
  */
 router.get('/:grub', function(req, res) {
-  res.render('grubs', { grub: req.grub});
+  res.render('grubs', { currentUser: req.session.passport.user, grub: req.grub});
 });
 
 /**
