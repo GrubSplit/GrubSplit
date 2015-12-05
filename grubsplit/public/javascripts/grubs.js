700526f5a4cfdc976b8009aa223fe842be0a97e9
Amanda Zhou <aezhou@mit.edu>
2015-12-05 14:17:39 -0500

    removing comments

--- grubsplit/public/javascripts/grubs.js
+++ grubsplit/public/javascripts/grubs.js
@@ -10,7 +10,6 @@ author: aezhou
 	// Change subgrub's paid status
 	$(document).on('click', '.togglePaid', function(evt) {
 		var toggleButton = $(this)
-		console.log(toggleButton)
 		var subgrubContainer = toggleButton.parent().parent().parent()
 		var url = '/subgrubs/payment/' + toggleButton.attr('subgrubid')
 		var payAction = false;
@@ -24,21 +23,18 @@ author: aezhou
 			{ markAsPaid: payAction }
 		).done(function(res) {
 			if (payAction) {
-				console.log('changing css to being paid')
 				subgrubContainer.addClass('hasPaid');
 				subgrubContainer.removeClass('hasNotPaid');
 				toggleButton.removeClass('btn-success');
 				toggleButton.addClass('btn-danger');
 				toggleButton.text('Mark As Unpaid');
 			} else {
-				console.log('changing css to being unpaid')
 				subgrubContainer.removeClass('hasPaid');
 				subgrubContainer.addClass('hasNotPaid');
 				toggleButton.addClass('btn-success');
 				toggleButton.removeClass('btn-danger');
 				toggleButton.text('Mark As Paid');
 			}
-			// window.location.replace(res)
 			return;
 		}).fail(function(resObj) {
 			alert('Unable to save payment status change - please try again later!');
