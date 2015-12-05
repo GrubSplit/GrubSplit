388c8b6df6f217f096b2fac72a6b48bbac100226
Jorrie Brettin <jorriebrettin@gmail.com>
2015-12-04 22:57:35 -0500

    jade bug fix

--- grubsplit/public/javascripts/checkout.js
+++ grubsplit/public/javascripts/checkout.js
@@ -1,4 +1,6 @@
-require('../../libraries/Delivery');
+// public javascript can't do direct calls to anything outside public. That needs to be done
+// in routes, so you'll have to make an ajax call to get whatever Delivery stuff you need.
+// require('../../libraries/Delivery');
 
 (function() {
   alert('HELLO');
