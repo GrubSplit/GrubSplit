dde0e18349798d21c851720a49af8c0ebdb346b4
Amanda Zhou <aezhou@mit.edu>
2015-12-05 12:32:02 -0500

    fixed class problem, move logic to outer div

--- grubsplit/public/javascripts/grubs.js
+++ grubsplit/public/javascripts/grubs.js
@@ -7,17 +7,15 @@ author: aezhou
 */
 
 (function() {
-
 	// Change subgrub's paid status
 	$(document).on('click', '.togglePaid', function(evt) {
-		console.log('button clicked oh hello')
-		var url = '/subgrubs'
-		var payAction = false;
 		var toggleButton = $(this)
-		if (toggleButton.hasClass('.hasPaid')) {
-			url += '/unpay/' + $(this).attr('subgrubid');
-		} else if ($(this).hasClass('.hasNotPaid')) {
-			url += '/pay/' + $(this).attr('subgrubid');
+		console.log(toggleButton)
+		var subgrubContainer = toggleButton.parent().parent().parent()
+		var url = '/subgrubs/payment/' + toggleButton.attr('subgrubid')
+		var payAction = false;
+
+		if (toggleButton.text() === 'Mark As Paid') {
 			payAction = true;
 		}
 
@@ -25,14 +23,22 @@ author: aezhou
 			url,
 			{ markAsPaid: payAction }
 		).done(function(res) {
-			if (toggleButton.hasClass('.hasPaid')) {
-				toggleButton.removeClass('.hasPaid');
-				toggleButton.addClass('.hasNotPaid')
-			} else if ($(this).hasClass('.hasNotPaid')) {
-				toggleButton.addClass('.hasPaid');
-				toggleButton.removeClass('.hasNotPaid')
+			if (payAction) {
+				console.log('changing css to being paid')
+				subgrubContainer.addClass('hasPaid');
+				subgrubContainer.removeClass('hasNotPaid');
+				toggleButton.removeClass('btn-success');
+				toggleButton.addClass('btn-danger');
+				toggleButton.text('Mark As Unpaid');
+			} else {
+				console.log('changing css to being unpaid')
+				subgrubContainer.removeClass('hasPaid');
+				subgrubContainer.addClass('hasNotPaid');
+				toggleButton.addClass('btn-success');
+				toggleButton.removeClass('btn-danger');
+				toggleButton.text('Mark As Paid');
 			}
-			window.location.replace(res);
+			// window.location.replace(res)
 			return;
 		}).fail(function(resObj) {
 			alert('Unable to save payment status change - please try again later!');
