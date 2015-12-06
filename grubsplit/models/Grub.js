01d4da632558beb100d4fd06a2ae9ff5cbb3f3a0
Amanda Zhou <aezhou@mit.edu>
2015-12-06 12:46:26 -0500

    removing unnecessary methods from Grub model
    update tax/tip/delivery

--- grubsplit/models/Grub.js
+++ grubsplit/models/Grub.js
@@ -37,9 +37,10 @@ var grubSchema = new mongoose.Schema({
 
 
 /*
-  given a grubID, return the grub, with all subgrubs populated
+  Create a new Grub, given a userID, and restaurant information
   @param: userID = mongoID of userID
   @param: restaurantID = mongo ObjectID of restaurant
+  @param: restaurantName = name of restaurant (string)
   @param: callback(err, grub)
 */
 grubSchema.statics.createNewGrub = function(userID, restaurantID, restaurantName, callback) {
@@ -74,55 +75,6 @@ grubSchema.statics.getGrub = function(grubID, callback) {
       });
     } else {
       callback({msg: 'could not find grub'});
-    }
-  });
-}
-
-/*
-  given a grub ID, update the tax, and return the grub or error message
-  @param: grubID = id of the grub
-  @param: tax = tax amount
-  @param: callback(err, grub)
-*/
-grubSchema.statics.updateTax = function(grubID, tax, callback) {
-  Grub.findOneAndUpdate({_id: grubID}, {$set: {tax: tax}}, {new: true}, function(err, grub) {
-    if (grub) {
-      callback(null, grub);
-    } else {
-      callback({msg: "could not find grub to update tax"});
-    }
-  });
-}
-
-
-/*
-  given a grub ID, update the delivery amount, and return the grub or error message
-  @param: grubID = id of the grub
-  @param: delivery = delivery fee
-  @param: callback(err, grub)
-*/
-grubSchema.statics.updateDelivery = function(grubID, delivery, callback) {
-  Grub.findOneAndUpdate({_id: grubID}, {$set: {delivery_fee: delivery}}, {new: true}, function(err, grub) {
-    if (grub) {
-      callback(null, grub);
-    } else {
-      callback({msg: "could not find grub to update delivery fee"});
-    }
-  });
-}
-
-/*
-  given a grub ID, update the tip, and return the grub or error message
-  @param: grubID = id of the grub
-  @param: tip = tip amount
-  @param: callback(err, grub)
-*/
-grubSchema.statics.updateTip = function(grubID, tip, callback) {
-  Grub.findOneAndUpdate({_id: grubID}, {$set: {tip: tip}}, {new: true}, function(err, grub) {
-    if (grub) {
-      callback(null, grub);
-    } else {
-      callback({msg: "could not find grub to update tip"});
     }
   });
 }
