3b80c6e6b0bd374d87488e36a8d7083c6a5eb702
Matthew Miklasevich <mattmik@mit.edu>
2015-12-06 18:29:02 -0500

    changed redirect_uri back

--- grubsplit/routes/grubs.js
+++ grubsplit/routes/grubs.js
@@ -89,7 +89,6 @@ router.post('/:grub/order', function(req, res) {
       order.push(item);
     });
   });
-  console.log(order);
   Delivery.createCart(req.grub.restaurantID, order, req.user.token, function(err, body) {
     if (err) {
       req.flash('errors', err);
